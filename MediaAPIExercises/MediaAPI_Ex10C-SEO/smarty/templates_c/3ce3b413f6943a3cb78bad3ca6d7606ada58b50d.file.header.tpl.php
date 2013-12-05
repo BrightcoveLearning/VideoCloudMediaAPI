248c03efc7eea1deb865f8170b2c33024c612415
doktorj7 <doktorjeff@Jeff-T-Doktors-iMac.local>
2013-12-05 14:57:01 -0700

    New Exercise samples

---
+++ MediaAPIExercises/MediaAPI_Ex10C-SEO/smarty/templates_c/3ce3b413f6943a3cb78bad3ca6d7606ada58b50d.file.header.tpl.php
@@ -0,0 +1,35 @@
+<?php /* Smarty version Smarty-3.1.11, created on 2013-11-27 15:15:02
+         compiled from "smarty/templates/header.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:85106686352966ee638df95-75456468%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '3ce3b413f6943a3cb78bad3ca6d7606ada58b50d' => 
+    array (
+      0 => 'smarty/templates/header.tpl',
+      1 => 1385137424,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '85106686352966ee638df95-75456468',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.11',
+  'unifunc' => 'content_52966ee63922d5_35587104',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_52966ee63922d5_35587104')) {function content_52966ee63922d5_35587104($_smarty_tpl) {?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Media API Exercise</title>
+				<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<link rel="stylesheet" type="text/css" href="MediaAPI.css">
+		<!-- Javascript Media API wrapper from opensource.brightcove.com -->
+		<script type="text/javascript" src="http://files.brightcove.com/bc-mapi.js"></script>
+		
+	</head>
+	<body>
+		<h2>Media API Training</h2>	
+<?php }} ?>
